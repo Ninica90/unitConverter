056f37e8e5a8e77d5bb0ff3a164c6a573e8a24c4
NikolinaKordic <nkordic02@gmail.com>
2018-11-25 21:30:26 +0100

    added definition of function and float(input) replaced with raw_input

--- unitConverter.py
+++ unitConverter.py
@@ -1,20 +1,19 @@
-print "Welcome to programme that coverts kilometres into miles. All you have to do is enter the desired distance in kilometres and programme will convertit to miles itself."
-"""
+#---------------------------------------------------------------------------
 def kmToMiles():                              # ako zelim definirati funkciju da se ne ponavlja ista vrijednost varijable u while petlji
-	km = float(input("Enter the value in kilometres: "))
-	print "Your value counts " + str(km * 1.609344) + " miles."
-"""
-#kmToMiles()
-km = float(input("Enter the value in kilometres: "))
-print "Your value counts " + str(km * 0.6213) + " miles."
+	km = raw_input("Enter the value in kilometres: ")
+	km_replace = km.replace(",", ".")
+	km = float(km_replace)
+	print "Your value counts " + str(km * 0.6213) + " miles."
+#----------------------------------------------------------------------------
+print "Welcome to programme that coverts kilometres into miles. All you have to do is enter the desired distance in kilometres and programme will convertit to miles itself."
+kmToMiles() # poziv funkcije
+
 POTVRDA = "Y"
 
 while True:
 	answer = raw_input("Do you want to enter new value? Press Y if yes and N if no: ")
 	if answer.upper() == POTVRDA:
-		#kmToMiles()
-		km = float(input("Enter the value in kilometres: "))
-		print "Your value counts " + str(km * 1.609344) + " miles."
+		kmToMiles()		
 	else:
 		print "Goodbye"
 		break
